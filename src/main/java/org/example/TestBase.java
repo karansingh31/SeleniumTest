96ca6ccbaed30119a9cb5c7e47540c4a153c8a64
karansingh31 <ksingh.saluja@gmail.com>
2024-01-19 00:44:45 +0530

    testing comments added

--- src/main/java/org/example/TestBase.java
+++ src/main/java/org/example/TestBase.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+//Testing comments
 public class TestBase {
     public static void main(String[] args) throws InterruptedException {
 
